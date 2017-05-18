fe7503816a6b4d8248e4e618b94691c3cd779aba
LEO <duguzixiao@icloud.com>
2017-05-18 15:00:42 +0800

    评论组件-列表数据展示功能

--- src/components/subcom/comment.vue
+++ src/components/subcom/comment.vue
@@ -1,10 +1,28 @@
 <template>
     <div id="tmpl">
+        <!--评论组件-->
+        <!--1.0 实现提交评论数据到服务器的静态结构-->
         <div id="postcomment">
             <h3>提交评论</h3>
             <p class="p"></p>
             <textarea placeholder="请输入您要评论的内容..." v-model="postcontent"></textarea>
             <mt-button type="primary" size="large" @click="postcomment">发表</mt-button>
+        </div>
+    
+        <!--2.0 实现获取评论数据列表-->
+        <div id="list">
+            <h3>评论列表</h3>
+            <p class="p"></p>
+            <div v-for="(item,index) in list">
+                <div class="title">
+                    <span>第{{index + 1}}楼:</span>
+                    <span>用户：{{item.user_name}}</span>
+                    <span>发表时间：{{item.add_time | datefmt('YYYY-MM-DD HH:mm:ss')}}</span>
+                </div>
+                <ul class="mui-table-view">
+                    <li class="mui-table-view-cell" v-text="item.content"></li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -16,8 +34,31 @@ export default {
     props: ['id'],//用来接收父组件传递过来的值
     data() {
         return {
-            postcontent : '',  //用来自动获取用户填写的评论内容
+            postcontent: '',  //用来自动获取用户填写的评论内容
+            list: []  //评论数据的数组
         };
+    },
+    created() {
+        // 静态数据
+        // this.list = [
+        //     {
+        //         "user_name": "匿名用户",
+        //         "add_time": "2017-05-10T01:08:16.000Z",
+        //         "content": " 满意度um"
+        //     },
+        //     {
+        //         "user_name": "匿名用户",
+        //         "add_time": "2017-05-08T06:32:40.000Z",
+        //         "content": "undefined"
+        //     },
+        //     {
+        //         "user_name": "匿名用户",
+        //         "add_time": "2017-05-08T06:23:57.000Z",
+        //         "content": "undefined"
+        //     }
+        // ]
+        // 获取当前资讯数据的评论信息列表
+        this.getcommentlist(1);
     },
     methods: {
         //1.0 评论数据的提交
@@ -37,7 +78,22 @@ export default {
                 //3.0 将文本框中的评论内容清空
                 this.postcontent = '';
             });
-        }
+        },
+        // 2.0 获取当前数据的评论数据 pageindex:代表的是当前获取的是哪一页的数据，默认值是1
+        getcommentlist(pageindex) {
+            pageindex = pageindex || 1;
+            // 1.0 确定评论数据的url
+            var url = common.apidomain + '/api/getcomments/' + this.id + '?pageindex=' + pageindex;
+            // 2.0 发出ajax请求获取数据即可
+            this.$http.get(url).then(function (res) {
+                if (res.body.status != 0) {
+                    Toast(res.body.message);
+                    return;
+                }
+                // 3.0 将message数组中的数据赋值给this.list
+                this.list = this.list.concat(res.body.message);
+            });
+        },
     }
 }
 </script>
@@ -52,5 +108,22 @@ export default {
     height: 1px;
     width: 100%;
     border-bottom: 1px solid rgba(0, 0, 0, 0.3);
+}
+
+
+
+
+
+/*2.0 评论列表的样式*/
+
+#list {
+    padding: 5px;
+}
+
+.title {
+    padding: 5px;
+    color: #6d6d72;
+    font-size: 15px;
+    background-color: rgba(0, 0, 0, 0.1);
 }
 </style>
