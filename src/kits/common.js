5a7ec39a884381b9a0817e78b78179b82624f805
LEO <duguzixiao@icloud.com>
2017-06-30 18:05:13 +0800

    切换根域名地址为lovegf.cn

--- src/kits/common.js
+++ src/kits/common.js
@@ -1,5 +1,6 @@
 // 系统中所有的公共配置
 
 export default{
-    apidomain:'http://webhm.top:8899', // 所有数据请求的根域名地址
+    // apidomain:'http://webhm.top:8899', // 所有数据请求的根域名地址
+    apidomain:'http://www.lovegf.cn:8899', // 所有数据请求的根域名地址
 }
